4af1ef72da6590dd57da92b75a695e34da376b70
hy-ws <h-y-bvb-unite@outlook.jp>
2019-07-29 09:25:29 +0900

    add CUDA classifier

---
+++ lib/classifier/classifier.cu
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+__host__ void classifier(float *input, int isize, float *output, int osize,
+        float *weight, float *bias) {
+    int i, j;
+
+    printf("Classifier:\n");
+    printf("  isize=%d, osize=%d\n", isize, osize);
+
+    for (i = 0; i < osize; i++) {
+        *(output+i) = 0.0;
+
+        for(j = 0; j < isize; j++) {
+            *(output+i) += *(weight+i*isize+j) * *(input+j);
+        }
+        *(output+i) += *(bias+i);
+    }
+    printf("\n");fflush(stdout);
+}
+
+
+
+__global__ void classifier(float *input, int isize, float *output, int osize,
+        float *weight, float *bias, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int j;
+
+    *(output + idx) = 0.0;
+
+    for (j = 0; j < isize; j++) {
+        *(output+idx) += *(weight+idx*isize+j) * *(input+j);
+    }
+    *(output+idx) += *(bias+idx);
+}
---
+++ lib/classifier/test_classifier.cu
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "test_header.h"
+
+
+#define GRID 512
+
+float get_rand()
+{
+    return (float)(rand() % 10) / 100;
+}
+
+int main(void)
+{
+    float *data_h, *data_d, *out_h, *out_d, *out_h_from_d;
+    int stride = 1;
+    int iN = 128;
+    int oN = 64;
+    int iNBytes = iN * sizeof(float), oNBytes = oN * sizeof(float);
+
+    float *w_h, *w_d, *b_h, *b_d;
+
+    // Memory allocate
+    data_h = (float *)malloc(iNBytes);
+    out_h = (float *)malloc(oNBytes);
+    out_h_from_d = (float *)malloc(oNBytes);
+    w_h = (float*)malloc(iN*oN*sizeof(float));
+    b_h = (float*)malloc(oN*sizeof(float));
+
+    // Initialize
+    for (int i = 0; i < iN; i++) {
+        data_h[i] = get_rand(); 
+    }
+
+    for (int i = 0; i < och*ksize*ksize; i++) {
+        w_h[i] = get_rand();
+    }
+
+
+    for (int i = 0; i < och; i++) {
+        b_h[i] = get_rand();
+    }
+
+    // CUDA memory allocate
+    cudaMalloc((void **)&data_d, iNBytes);
+    cudaMalloc((void **)&out_d, oNBytes);
+    cudaMalloc((void **)&w_d, iN*oN*sizeof(float));
+    cudaMalloc((void **)&b_d, oN*sizeof(float));
+
+    cudaMemcpy(data_d, data_h, iNBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(w_d, w_h, iN*oN*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b_h, oN*sizeof(float), cudaMemcpyHostToDevice);
+
+    // Execute
+    classifier(data_h, iN, data_h, oN, w_h, b_h)
+    classifier <<< GRID, oN / GRID + 1 >>> (data_d, iN, data_d, oN, w_d, b_d, oN)
+
+    cudaMemcpy(out_h_from_d, out_d, oN * sizeof(float), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < oN; i++) {
+        printf("i: %d, CPU: %3.3f, GPU: %3.3f\n", i, out_h[i], out_h_from_d[i]);
+        assert(abs(out_h[i] - out_h_from_d[i]) < 0.001);
+    }
+    printf("%d\n", oN);
+
+    // Free 
+    free(data_h); free(out_h); free(out_h_from_d); free(w_h); free(b_h);
+    // CUDA free
+    cudaFree(data_d), cudaFree(out_d), cudaFree(w_d), cudaFree(b_d);
+
+    printf("Success Test\n");
+    return 0;
+}
